1dd29c4d5bf8571ab36d5be9db04f11c054ce3df
Lv Jingxin <lv510987@163.com>
2024-03-05 18:13:06 +0800

    feat: 初始化项目

---
+++ src/index.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: Lv Jingxin lv510987@163.com
+ * @Date: 2024-03-05 17:48:56
+ * @LastEditors: Lv Jingxin lv510987@163.com
+ * @LastEditTime: 2024-03-05 17:58:33
+ * @FilePath: /react-practice/src/index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
