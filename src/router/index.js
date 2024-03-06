e91f9e8f86dc29eac6f2675c6c5c96dd400a7b5b
Lv Jingxin <lv510987@163.com>
2024-03-06 09:36:57 +0800

    feat: 配置联想提示@/

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@
  * @Author: Lv Jingxin lv510987@163.com
  * @Date: 2024-03-06 09:06:52
  * @LastEditors: Lv Jingxin lv510987@163.com
- * @LastEditTime: 2024-03-06 09:30:49
+ * @LastEditTime: 2024-03-06 09:36:29
  * @FilePath: /react-practice/src/router/index.js
  * @Description: 路由配置
  */
