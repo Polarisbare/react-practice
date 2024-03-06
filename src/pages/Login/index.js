666a8f7d31c9c986769033dc6b65e056a5b12628
Lv Jingxin <lv510987@163.com>
2024-03-06 09:59:02 +0800

    feat: 搭建登录页表单结构

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,11 +2,41 @@
  * @Author: Lv Jingxin lv510987@163.com
  * @Date: 2024-03-06 09:05:09
  * @LastEditors: Lv Jingxin lv510987@163.com
- * @LastEditTime: 2024-03-06 09:06:22
+ * @LastEditTime: 2024-03-06 09:57:34
  * @FilePath: /react-practice/src/pages/Login/index.js
  * @Description: Login
  */
+import { Card, Form, Input, Checkbox, Button } from "antd";
+import logo from "@/assets/logo.png";
+import "./index.scss";
+
 const Login = () => {
-  return <div>this is Login</div>;
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        <Form>
+          <Form.Item>
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item>
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Checkbox className="login-checkbox-label">
+              我已阅读并同意「用户协议」和「隐私条款」
+            </Checkbox>
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
 };
+
 export default Login;
