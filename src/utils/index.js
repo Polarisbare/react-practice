1faa19d4270b1a1563ac4df43e0af964604d2579
Lv Jingxin <lv510987@163.com>
2024-03-06 10:25:17 +0800

    feat: 封装axios

---
+++ src/utils/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: Lv Jingxin lv510987@163.com
+ * @Date: 2024-03-06 10:22:53
+ * @LastEditors: Lv Jingxin lv510987@163.com
+ * @LastEditTime: 2024-03-06 10:24:11
+ * @FilePath: /react-practice/src/utils/index.js
+ * @Description: 统一中转模块函数 隔离机制
+ */
+import { request } from "./request";
+export { request };
---
+++ src/utils/request.js
@@ -0,0 +1,40 @@
+/*
+ * @Author: Lv Jingxin lv510987@163.com
+ * @Date: 2024-03-06 10:18:41
+ * @LastEditors: Lv Jingxin lv510987@163.com
+ * @LastEditTime: 2024-03-06 10:22:27
+ * @FilePath: /react-practice/src/utils/request.js
+ * @Description:  axios封装
+ */
+// axios 封装
+import axios from "axios";
+
+// 1. 跟域名配置
+// 2. 超时时间
+const request = axios.create({
+  baseURL: "http://geek.itheima.net/v1_0",
+  timeout: 5000,
+});
+
+// 3. 请求拦截器
+request.interceptors.request.use(
+  (config) => {
+    // Do something before request is sent
+    return config;
+  },
+  (error) => {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+//4. 响应拦截器
+request.interceptors.response.use(
+  (response) => {
+    // Do something before response is sent
+    return response;
+  },
+  (error) => {
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
