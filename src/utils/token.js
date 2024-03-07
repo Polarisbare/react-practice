d936ff760231e4dad8ea9818adc25f68039d5252
Lv Jingxin <lv510987@163.com>
2024-03-07 09:05:48 +0800

    feat: token本地化

---
+++ src/utils/token.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: Lv Jingxin lv510987@163.com
+ * @Date: 2024-03-07 09:04:05
+ * @LastEditors: Lv Jingxin lv510987@163.com
+ * @LastEditTime: 2024-03-07 09:05:30
+ * @FilePath: /react-practice/src/utils/token.js
+ * @Description: 封装和token相关的方法
+ */
+function setToken() {}
+function getToken() {}
+function retmoveToken() {}
+export { setToken, getToken, retmoveToken };
