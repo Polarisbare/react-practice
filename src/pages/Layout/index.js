ceca26c9fa3d55414cda2394b650f851568ff4e3
Lv Jingxin <lv510987@163.com>
2024-03-08 09:14:03 +0800

    feat: 初始化样式

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -2,17 +2,70 @@
  * @Author: Lv Jingxin lv510987@163.com
  * @Date: 2024-03-06 09:04:51
  * @LastEditors: Lv Jingxin lv510987@163.com
- * @LastEditTime: 2024-03-07 09:26:30
+ * @LastEditTime: 2024-03-08 09:13:35
  * @FilePath: /react-practice/src/pages/Layout/index.js
  * @Description: Layout
  */
 // 测试token
-import { request } from "@/utils";
-import { useEffect } from "react";
-const Layout = () => {
-  useEffect(() => {
-    request.get("user/profile");
-  }, []);
-  return <div>this is layout</div>;
+import { Layout, Menu, Popconfirm } from "antd";
+import {
+  HomeOutlined,
+  DiffOutlined,
+  EditOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons";
+import "./index.scss";
+import "normalize.css";
+
+const { Header, Sider } = Layout;
+
+const items = [
+  {
+    label: "首页",
+    key: "1",
+    icon: <HomeOutlined />,
+  },
+  {
+    label: "文章管理",
+    key: "2",
+    icon: <DiffOutlined />,
+  },
+  {
+    label: "创建文章",
+    key: "3",
+    icon: <EditOutlined />,
+  },
+];
+
+const GeekLayout = () => {
+  return (
+    <Layout>
+      <Header className="header">
+        <div className="logo" />
+        <div className="user-info">
+          <span className="user-name">练习</span>
+          <span className="user-logout">
+            <Popconfirm title="是否确认退出？" okText="退出" cancelText="取消">
+              <LogoutOutlined /> 退出
+            </Popconfirm>
+          </span>
+        </div>
+      </Header>
+      <Layout>
+        <Sider width={200} className="site-layout-background">
+          <Menu
+            mode="inline"
+            theme="dark"
+            defaultSelectedKeys={["1"]}
+            items={items}
+            style={{ height: "100%", borderRight: 0 }}
+          ></Menu>
+        </Sider>
+        <Layout className="layout-content" style={{ padding: 20 }}>
+          内容
+        </Layout>
+      </Layout>
+    </Layout>
+  );
 };
-export default Layout;
+export default GeekLayout;
