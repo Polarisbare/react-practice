3cab21f60c843d008bae352ac8a8c4e0a99ecc1b
Lv Jingxin <lv510987@163.com>
2024-03-11 09:57:46 +0800

    feat: 封装echarts组件

---
+++ src/pages/Home/components/BarChart.js
@@ -0,0 +1,37 @@
+/*
+ * @Author: Lv Jingxin lv510987@163.com
+ * @Date: 2024-03-11 09:48:56
+ * @LastEditors: Lv Jingxin lv510987@163.com
+ * @LastEditTime: 2024-03-11 09:54:45
+ * @FilePath: /react-practice/src/pages/Home/components/BarChart.js
+ * @Description: 柱状图组件
+ */
+import * as echarts from "echarts";
+import { useEffect, useRef } from "react";
+const BarChart = ({ title }) => {
+  const chartRef = useRef(null);
+  useEffect(() => {
+    const chartDom = chartRef.current;
+    const myChart = echarts.init(chartDom);
+    const option = {
+      title: { text: title },
+      xAxis: {
+        type: "category",
+        data: ["Vue", "React", "Go"],
+      },
+      yAxis: {
+        type: "value",
+      },
+      series: [
+        {
+          data: [10, 40, 70, 80, 100, 110, 130],
+          type: "bar",
+        },
+      ],
+    };
+    // 使用图表参数完成渲染
+    option && myChart.setOption(option);
+  }, []);
+  return <div ref={chartRef} style={{ width: "500px", height: "400px" }}></div>;
+};
+export default BarChart;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,39 +2,16 @@
  * @Author: Lv Jingxin lv510987@163.com
  * @Date: 2024-03-08 09:16:15
  * @LastEditors: Lv Jingxin lv510987@163.com
- * @LastEditTime: 2024-03-11 09:45:32
+ * @LastEditTime: 2024-03-11 09:55:08
  * @FilePath: /react-practice/src/pages/Home/index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import * as echarts from "echarts";
-import { useEffect, useRef } from "react";
-
+import BarChart from "./components/BarChart";
 const Home = () => {
-  const chartRef = useRef(null);
-  useEffect(() => {
-    const chartDom = chartRef.current;
-    const myChart = echarts.init(chartDom);
-    const option = {
-      xAxis: {
-        type: "category",
-        data: ["Vue", "React", "Go"],
-      },
-      yAxis: {
-        type: "value",
-      },
-      series: [
-        {
-          data: [10, 40, 70, 80, 100, 110, 130],
-          type: "bar",
-        },
-      ],
-    };
-    // 使用图表参数完成渲染
-    option && myChart.setOption(option);
-  }, []);
   return (
     <div>
-      <div ref={chartRef} style={{ width: "500px", height: "400px" }}></div>
+      <BarChart title={"三大框架满意度"} />
+      <BarChart title={"哈哈哈哈大框架满意度"} />
     </div>
   );
 };
