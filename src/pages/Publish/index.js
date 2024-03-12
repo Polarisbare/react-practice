94c12f0435d5d8df26d3eee7a698c48a18808b3a
Lv Jingxin <lv510987@163.com>
2024-03-12 09:32:49 +0800

    feat: 发布文章基础搭建

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -2,11 +2,80 @@
  * @Author: Lv Jingxin lv510987@163.com
  * @Date: 2024-03-08 09:18:02
  * @LastEditors: Lv Jingxin lv510987@163.com
- * @LastEditTime: 2024-03-08 09:18:23
+ * @LastEditTime: 2024-03-12 09:32:05
  * @FilePath: /react-practice/src/pages/Publish/index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
+import {
+  Card,
+  Breadcrumb,
+  Form,
+  Button,
+  Radio,
+  Input,
+  Upload,
+  Space,
+  Select,
+} from "antd";
+import { PlusOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+import "./index.scss";
+
+const { Option } = Select;
+
 const Publish = () => {
-  return <div>Publish</div>;
+  return (
+    <div className="publish">
+      <Card
+        title={
+          <Breadcrumb
+            items={[
+              { title: <Link to={"/"}>首页</Link> },
+              { title: "发布文章" },
+            ]}
+          />
+        }
+      >
+        <Form
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ type: 1 }}
+        >
+          <Form.Item
+            label="标题"
+            name="title"
+            rules={[{ required: true, message: "请输入文章标题" }]}
+          >
+            <Input placeholder="请输入文章标题" style={{ width: 400 }} />
+          </Form.Item>
+          <Form.Item
+            label="频道"
+            name="channel_id"
+            rules={[{ required: true, message: "请选择文章频道" }]}
+          >
+            <Select placeholder="请选择文章频道" style={{ width: 400 }}>
+              <Option value={0}>推荐</Option>
+            </Select>
+          </Form.Item>
+          <Form.Item
+            label="内容"
+            name="content"
+            rules={[{ required: true, message: "请输入文章内容" }]}
+          >
+            {/* 富文本编辑器 */}
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 4 }}>
+            <Space>
+              <Button size="large" type="primary" htmlType="submit">
+                发布文章
+              </Button>
+            </Space>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
 };
+
 export default Publish;
