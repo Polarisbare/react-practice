cd557fa82b67adc92fd3de8ce609e6d59e036490
Lv Jingxin <lv510987@163.com>
2024-03-14 10:25:40 +0800

    feat: 分页

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -2,7 +2,7 @@
  * @Author: Lv Jingxin lv510987@163.com
  * @Date: 2024-03-08 09:17:25
  * @LastEditors: Lv Jingxin lv510987@163.com
- * @LastEditTime: 2024-03-14 10:11:52
+ * @LastEditTime: 2024-03-14 10:23:31
  * @FilePath: /react-practice/src/pages/Article/index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -139,7 +139,13 @@ const Article = () => {
     });
     // 重新渲染table useEffect依赖项变化也变化
   };
-
+  const onPageChange = (page) => {
+    console.log("=========>", page);
+    setReqData({
+      ...reqData,
+      page,
+    });
+  };
   return (
     <div>
       <Card
@@ -190,7 +196,16 @@ const Article = () => {
       </Card>
       {/* 表格区域 */}
       <Card title={`根据筛选条件共查询到 ${count} 条结果：`}>
-        <Table rowKey="id" columns={columns} dataSource={list} />
+        <Table
+          rowKey="id"
+          columns={columns}
+          dataSource={list}
+          pagination={{
+            total: count,
+            pageSize: reqData.per_page,
+            onChange: onPageChange,
+          }}
+        />
       </Card>
     </div>
   );
