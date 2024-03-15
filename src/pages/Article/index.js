8efd10f57b867fc73870e60b440929dadd8de139
Lv Jingxin <lv510987@163.com>
2024-03-15 10:10:21 +0800

    feat: 优化

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -2,7 +2,7 @@
  * @Author: Lv Jingxin lv510987@163.com
  * @Date: 2024-03-08 09:17:25
  * @LastEditors: Lv Jingxin lv510987@163.com
- * @LastEditTime: 2024-03-15 09:31:04
+ * @LastEditTime: 2024-03-15 10:02:51
  * @FilePath: /react-practice/src/pages/Article/index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -194,7 +194,7 @@ const Article = () => {
           <Form.Item label="频道" name="channel_id">
             <Select
               placeholder="请选择文章频道"
-              defaultValue="lucy"
+              defaultValue={Form.initialValues}
               style={{ width: 120 }}
             >
               {channelList.map((item) => (
--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -2,7 +2,7 @@
  * @Author: Lv Jingxin lv510987@163.com
  * @Date: 2024-03-08 09:18:02
  * @LastEditors: Lv Jingxin lv510987@163.com
- * @LastEditTime: 2024-03-15 09:54:19
+ * @LastEditTime: 2024-03-15 10:00:02
  * @FilePath: /react-practice/src/pages/Publish/index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -82,6 +82,8 @@ const Publish = () => {
         })
       );
     }
+    // 回填 如果有id就调用
+    if (!articleId) return;
     getArticleDetail();
   }, [articleId, form]);
   return (
